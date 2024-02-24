407ee6d0ca357fd4ac9b865ec1f9436114813474
Nikita <niki.c2000@yandex.ru>
2024-02-24 16:32:47 +0400

    добавлен контроллер основной страницы

---
+++ backendSiteDiplom/src/main/java/com/AlertSystem/backendSiteDiplom/controllers/MainPage.java
@@ -0,0 +1,78 @@
+package com.AlertSystem.backendSiteDiplom.controllers;
+
+import com.AlertSystem.backendSiteDiplom.dto.CourseDTO;
+import com.AlertSystem.backendSiteDiplom.models.Course;
+import com.AlertSystem.backendSiteDiplom.models.People;
+import com.AlertSystem.backendSiteDiplom.models.PeopleInCourse;
+import com.AlertSystem.backendSiteDiplom.models.PeopleInCourseId;
+import com.AlertSystem.backendSiteDiplom.services.CourseService;
+import com.AlertSystem.backendSiteDiplom.services.PeopleInCourseService;
+import com.AlertSystem.backendSiteDiplom.services.PeopleService;
+import com.AlertSystem.backendSiteDiplom.util.JWTUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:5500", allowCredentials = "true")
+@RestController
+@RequestMapping("/mainPage")
+public class MainPage {
+
+    private JWTUtil jwtUtil;
+    private PeopleService peopleService;
+    private CourseService courseService;
+    private PeopleInCourseService peopleInCourseService;
+
+    public MainPage(JWTUtil jwtUtil, PeopleService peopleService, CourseService courseService, PeopleInCourseService peopleInCourseService) {
+        this.jwtUtil = jwtUtil;
+        this.peopleService = peopleService;
+        this.courseService = courseService;
+        this.peopleInCourseService = peopleInCourseService;
+    }
+
+    @GetMapping("/userInfo")
+    public ResponseEntity<Map> userInfo(@CookieValue(value = "jwt-token") String jwtCookie) {
+        String login = jwtUtil.validateToken(jwtCookie);
+        People people = peopleService.getByLogin(login);
+
+        Map response = Map.of("login",people.getLogin(),
+                "name", people.getName(),
+                "serName", people.getSerName(),
+                "role", people.getRole(),
+                "email", people.getEmail());
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/CourseInfo")
+    public ResponseEntity<List> Courses(@CookieValue(value = "jwt-token") String jwtCookie) {
+        String login = jwtUtil.validateToken(jwtCookie);
+        People people = peopleService.getByLogin(login);
+        List<PeopleInCourse> courses = peopleInCourseService.findCoursesByPeopleId(people.getId());
+        return ResponseEntity.ok(courses);
+    }
+
+    @PostMapping("/createCourse")
+    public ResponseEntity<Map> createCourse(@CookieValue(value = "jwt-token") String jwtCookie,
+                                            @RequestBody CourseDTO courseDTO) {
+        String login = jwtUtil.validateToken(jwtCookie);
+        People people = peopleService.getByLogin(login);
+
+        Course course = new Course(courseDTO.getCourseName(),
+                courseDTO.getCourseDescription(),
+                people.getId());
+        courseService.saveCourse(course);
+        List<String> students = courseDTO.getStudents();
+        for (int i=0;i<students.size();i++) {
+            People student = peopleService.getByLogin(students.get(i));
+            PeopleInCourseId peopleInCourseId = new PeopleInCourseId(course.getId(), student.getId());
+            PeopleInCourse peopleInCourse = new PeopleInCourse();
+            peopleInCourse.setPeopleInCourseId(peopleInCourseId);
+            peopleInCourseService.savePeopleInCourse(peopleInCourse);
+        }
+
+        return ResponseEntity.ok(Map.of("status", "success"));
+    }
+
+}
---
+++ backendSiteDiplom/src/main/java/com/AlertSystem/backendSiteDiplom/dto/CourseDTO.java
@@ -0,0 +1,33 @@
+package com.AlertSystem.backendSiteDiplom.dto;
+
+import java.util.List;
+
+public class CourseDTO {
+    private String courseName;
+    private String courseDescription;
+    private List<String> students;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+
+    public void setCourseDescription(String courseDescription) {
+        this.courseDescription = courseDescription;
+    }
+
+    public List<String> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<String> students) {
+        this.students = students;
+    }
+}
